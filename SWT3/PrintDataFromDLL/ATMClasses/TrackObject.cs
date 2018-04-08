c95b5bd5fa186f63e54888cb73f377d89ce50426
Martin Andersen <martin_haugaard8@hotmail.com>
2018-04-08 22:39:21 +0200

    Tilføjede TrackObject class med implemtering

---
+++ SWT3/PrintDataFromDLL/ATMClasses/TrackObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMClasses
+{
+    public class TrackObject
+    {
+        public string Tag { get; set; }
+        public string XCoord { get; set; }
+        public string YCoord { get; set; }
+        public string Altitude { get; set; }
+        public string Timestamp { get; set; }
+        public string HVelocity { get; set; }
+        public string Direction { get; set; }
+    }
+}
