d4c1450a3426ee4ae10c3d3c72181759a11a9c49
Martin Andersen <martin_haugaard8@hotmail.com>
2018-04-09 12:29:07 +0200

    Ændringer i trackingvalidationtest

--- SWT3/PrintDataFromDLL/ATM.Tests.Unit/TrackingValidationTest.cs
+++ SWT3/PrintDataFromDLL/ATM.Tests.Unit/TrackingValidationTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace ATM.Tests.Unit
@@ -9,7 +10,16 @@ namespace ATM.Tests.Unit
         [SetUp]
         public void Setup()
         {
-            
+            var trackData = new List<string>();
+
+            trackData.Add("ATR423");
+            trackData.Add("39045");
+            trackData.Add("12932");
+            trackData.Add("14000");
+            trackData.Add("20151006213456789");
         }
+
+        [Test]
+        public void 
     }
 }
