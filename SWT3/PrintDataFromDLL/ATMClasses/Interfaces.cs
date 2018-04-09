df317a700ad2fb0429ab5ab33866c1c5f6c702c1
Søren Mathiasen <zuperzam@email.dk>
2018-04-09 11:47:20 +0200

    Initial creation of interfaces

---
+++ SWT3/PrintDataFromDLL/ATMClasses/Interfaces.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMClasses
+{
+    interface IPrint
+    {
+        void printTrack(TrackObject track);
+    }
+
+    interface ITextFormatter
+    {
+        void FormatText(string x);
+    }
+}
