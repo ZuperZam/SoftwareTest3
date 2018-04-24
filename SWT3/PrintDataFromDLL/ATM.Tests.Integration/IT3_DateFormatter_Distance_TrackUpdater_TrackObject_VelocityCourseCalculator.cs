529c433ed9c2998b7b4e9c1e43daeb25fedfa3f5
fred5954 <frederik.kastrup.mortensen@post.au.dk>
2018-04-24 23:19:54 +0200

    Tilføjet integrationtests

---
+++ SWT3/PrintDataFromDLL/ATM.Tests.Integration/IT3_DateFormatter_Distance_TrackUpdater_TrackObject_VelocityCourseCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.Tests.Integration
+{
+    class IT3_DateFormatter_Distance_TrackUpdater_TrackObject_VelocityCourseCalculator
+    {
+    }
+}
---
+++ SWT3/PrintDataFromDLL/ATM.Tests.Integration/IT5_DateFormatter_Distance_TrackUpdater_Print_TrackObject_VelocityCourseCalculator_ATMSystem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.Tests.Integration
+{
+    class IT5_DateFormatter_Distance_TrackUpdater_Print_TrackObject_VelocityCourseCalculator_ATMSystem
+    {
+    }
+}
