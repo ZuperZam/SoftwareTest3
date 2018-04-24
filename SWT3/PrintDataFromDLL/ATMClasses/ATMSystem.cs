52e59f1587a0aa8b203585300d7251f51418e049
Martin Andersen <martin_haugaard8@hotmail.com>
2018-04-24 08:57:47 +0200

    Tilføjede ATMSystem og TrackUpdater. TrackUpdater skal testes.

---
+++ SWT3/PrintDataFromDLL/ATMClasses/ATMSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATMClasses.Interfaces;
+
+namespace ATMClasses
+{
+    class ATMSystem
+    {
+        public ATMSystem(
+            ITrackListEvent trackListEvent)
+        {
+            trackListEvent.TrackListReady += OnTrackListReady;
+
+        }
+
+        private void OnTrackListReady(object sender, TrackListEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SWT3/PrintDataFromDLL/ATMClasses/TrackUpdater.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATMClasses.Interfaces;
+using Microsoft.Win32;
+
+namespace ATMClasses
+{
+    class TrackUpdater
+    {
+        private IVelocityCourseCalculator _velocityCourseCalculator;
+
+        public TrackUpdater(IVelocityCourseCalculator velocityCourseCalculator)
+        {
+            _velocityCourseCalculator = velocityCourseCalculator;
+        }
+
+        public void updateTracks(ref List<TrackObject> newTrackObjects, List<TrackObject> oldTrackObjects)
+        {
+            foreach (var newTrackObject in newTrackObjects)
+            {
+                foreach (var oldTrackObject in oldTrackObjects)
+                {
+                    if (newTrackObject.Tag == oldTrackObject.Tag)
+                    {
+                        newTrackObject.Velocity = _velocityCourseCalculator.CalculateVelocity(oldTrackObject, newTrackObject);
+                        newTrackObject.Course = _velocityCourseCalculator.CalculateCourse(oldTrackObject, newTrackObject);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
