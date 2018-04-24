435d0ada01ae6c3c984b535abb405241ee9e3eb2
fred5954 <frederik.kastrup.mortensen@post.au.dk>
2018-04-24 23:31:32 +0200

    nu

--- SWT3/PrintDataFromDLL/ATM.Tests.Integration/IT1_DateFormatter_TrackObject.cs
+++ SWT3/PrintDataFromDLL/ATM.Tests.Integration/IT1_DateFormatter_TrackObject.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ATMClasses;
+using ATMClasses.Interfaces;
+using NUnit;
 namespace ATM.Tests.Integration
 {
-    class IT1_DateFormatter_TrackObject
+    [TestFixture]
+    public class IT1_DateFormatter_TrackObject
     {
+        private IDateFormatter dateformatter;
+        private TrackObject _uut;
+
+
+
     }
 }
