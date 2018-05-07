2f7ccb31d47014ea65d4bcb85324f8bcd14730fb
fred5954 <frederik.kastrup.mortensen@post.au.dk>
2018-05-07 13:25:30 +0200

    Dateformatter tilføjet til TransponderParsing

--- SWT3/PrintDataFromDLL/ATMRefactored/TransponderParsing.cs
+++ SWT3/PrintDataFromDLL/ATMRefactored/TransponderParsing.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 using ATMRefactored;
 using ATMRefactored.Interfaces;
 using TransponderReceiver;
+using System.Globalization;
 
 namespace ATMRefactored
 {
     public class TransponderParsing : ITransponderParsing
     {
+        public string UglyTimestamp; 
         public TrackingFiltering trackingFiltering;
         private List<TrackObject> _trackObjects = new List<TrackObject>();
 
@@ -33,10 +35,30 @@ namespace ATMRefactored
 
                 var track = new TrackObject(trackData);
 
+                track.PrettyTimeStamp = FormatTimestamp(trackData[4]);
+
                 _trackObjects.Add(track);
             }
 
             trackingFiltering.IsTrackInMonitoredAirspace(_trackObjects);
+        }
+
+        public string FormatTimestamp(string timestamp)
+        {
+            string format = "yyyyMMddHHmmssfff";    //Set input format
+            DateTime date = DateTime.ParseExact(timestamp, format, CultureInfo.CreateSpecificCulture("en-US"));
+            string dateformat = String.Format(new CultureInfo("en-US"), "{0:MMMM d}{1}{0:, yyyy, 'at' HH:mm:ss 'and' fff 'milliseconds'}", date, GetDaySuffix(date));   //Format date correctly
+
+            return dateformat;
+        }
+
+        public string GetDaySuffix(DateTime timeStamp)
+        {
+            //returns "st", "nd", "rd" or "th"
+            return (timeStamp.Day % 10 == 1 && timeStamp.Day != 11) ? "st"
+                : (timeStamp.Day % 10 == 2 && timeStamp.Day != 12) ? "nd"
+                : (timeStamp.Day % 10 == 3 && timeStamp.Day != 13) ? "rd"
+                : "th";
         }
     }
 }
